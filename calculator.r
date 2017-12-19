f1878e8b2119c50abe82c760e570c54ecfdc2e9d
pgacode <paulgandrews@gmail.com>
2017-12-19 22:43:57 +0000

    Add files via upload

---
+++ calculator.r
@@ -0,0 +1,56 @@
+# Program make a simple calculator
+# that can add, subtract, multiply
+# and divide using functions
+
+add <- function(x, y) {
+  return(x + y)
+}
+
+subtract <- function(x, y) {
+  return(x - y)
+}
+
+multiply <- function(x, y) {
+  return(x * y)
+}
+
+divide <- function(x, y) {
+  return(x / y)
+}
+
+square <- function(x) {
+  return(x * x)
+}
+
+cubed <- function(x) {
+  return(x * x * x)
+}
+
+sqroot <- function(x) {
+  return(sqrt(x))
+}
+
+power <- function(x, y) {
+  return(power(x,y))
+}
+
+cosine <- function(x) {
+  return(cos(x))
+}
+
+factorialfunct <- function(x) {
+  return(factor(x))
+}
+
+
+add (5,5)
+subtract (10,-5)
+multiply (5,5)
+divide (10,2)
+square (5)
+cubed (2)
+sqroot (9)
+power (10,2)
+cos (100)
+factorialfunct(40)
+
